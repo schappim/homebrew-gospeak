61686729bf9d14dd8333602080e019a936da0262
Marcus Schappi <marcus@schappi.com>
2026-01-23 14:13:41 +1100

    Update to v0.3.1 - Fix audio cutoff

--- Formula/gospeak.rb
+++ Formula/gospeak.rb
@@ -1,8 +1,8 @@
 class Gospeak < Formula
   desc "CLI for text-to-speech using OpenAI, ElevenLabs, or Deepgram TTS API - no ffmpeg required"
   homepage "https://github.com/schappim/gospeak"
-  url "https://github.com/schappim/gospeak/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "28adb03d27d92cd5645316f4677972d667ca311aeca30fac298b969a84db1249"
+  url "https://github.com/schappim/gospeak/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "f40c3d2209093b963346efec69dc53e395e3617fec73974f3ed697645b2a38ef"
   license "MIT"
   head "https://github.com/schappim/gospeak.git", branch: "master"
 
